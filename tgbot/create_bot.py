10ef6f099f51cfd2b18c3e1f59481c222c79bce0
Igor Ter-Pogosyan <igor-526@yandex.ru>
2024-03-31 23:11:43 +0300

    parse mode aiogram

--- tgbot/create_bot.py
+++ tgbot/create_bot.py
@@ -3,5 +3,5 @@ from aiogram.enums import ParseMode
 import os
 
 dp = Dispatcher()
-# bot = Bot(token=os.environ.get('TELEGRAM_BOT_TOKEN'))
-bot = Bot(token="6323556516:AAHqCJra_LMYIDK5PkZV3-EebcCDZky7Qcs")
+bot = Bot(token=os.environ.get('TELEGRAM_BOT_TOKEN'),
+          parse_mode=ParseMode.HTML)
