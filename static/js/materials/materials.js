1c465d7288a70f0676a483c513ba16bd38f8b032
Igor Ter-Pogosyan <igor-526@yandex.ru>
2024-05-18 20:51:43 +0300

    animation preview bug 4

--- static/js/materials/materials.js
+++ static/js/materials/materials.js
@@ -151,7 +151,7 @@ function materialsMainCollapseAction(materialID, action="show"){
                         case "Изображение":
                             element.innerHTML = `<a href="${request.response.file}" target="_blank"><img src="${request.response.file}" class="img-fluid" alt="Изображение" style="max-height: 150px;"></a>`
                             break
-                        case "Анмиация":
+                        case "Анимация":
                             element.innerHTML = `<a href="${request.response.file}" target="_blank"><img src="${request.response.file}" class="img-fluid" alt="Изображение" style="max-height: 150px;"></a>`
                             break
                         default:
