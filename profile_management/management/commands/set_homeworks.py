14211568f0cab5e817f8a7360ceae012fd5c30ba
Igor Ter-Pogosyan <igor-526@yandex.ru>
2024-09-27 01:28:58 +0300

    util for setting homeworks

---
+++ profile_management/management/commands/set_homeworks.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+
+from homework.models import Homework, HomeworkLog
+from lesson.models import Lesson
+
+
+class Command(BaseCommand):
+    help = 'This command will setup groups and permissions'
+
+    def handle(self, *args, **kwargs):
+        counter = 0
+        lessons = Lesson.objects.filter(status=1)
+        for lesson in lessons:
+            homeworks = lesson.homeworks.all()
+            for homework in homeworks:
+                if homework.get_status().status == 0:
+                    homework.set_assigned()
+                    counter += 1
+        print(counter)
