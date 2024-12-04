2ac143bdffe21ed679dc2ac3c6ece6288f542315
Igor Ter-Pogosyan <igor-526@yandex.ru>
2024-12-04 20:01:07 +0300

    Mini App testing

--- static/js/telegram/ma/login.js
+++ static/js/telegram/ma/login.js
@@ -2,15 +2,17 @@ const fd = new FormData()
 fd.set("redirect_url", window.location.href)
 console.log(tgUserdata)
 if (tgUserdata.user){
-    fd.set("tg_url", tgUserdata.user.id)
+    fd.set("tg_id", tgUserdata.user.id)
 }
 console.log(tgUserdata)
-// fetch('/ma/login/', {
-//     method: 'POST',
-//     credentials: 'same-origin',
-//     body: fd
-// }).then(request => {
-//     if (request.status === 302) {
-//         // request.json().then(url => window.location.assign(url))
-//     }
-// })
+fetch('/ma/login/', {
+    method: 'POST',
+    credentials: 'same-origin',
+    body: fd
+}).then(request => {
+    if (request.status === 302) {
+        setTimeout(function () {
+                    request.json().then(url => window.location.assign(url))
+        }, 5000)
+    }
+})
