6cee73e6237923edc6febff47f3d9207ee85ffce
Igor Ter-Pogosyan <igor-526@yandex.ru>
2024-12-04 13:26:24 +0300

    Mini App testing

--- lesson/views_ma.py
+++ lesson/views_ma.py
@@ -1,9 +1,8 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
-class LessonMAReviewFormPage(LoginRequiredMixin, TemplateView):
+class LessonMAReviewFormPage(TemplateView):
     template_name = "ma/lesson_review_form.html"
 
     def get(self, request, *args, **kwargs):
