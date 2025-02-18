8561baa3a1848ce766282345c29b16fa327986a5
Igor Ter-Pogosyan <igor-526@yandex.ru>
2025-02-18 23:23:29 +0300

    celery2

--- learning_plan/views_api.py
+++ learning_plan/views_api.py
@@ -255,5 +255,5 @@ class PlansDownloadAPIView(CanDownloadPlan, APIView):
             type=1,
             initiator=request.user,
         )
-        plans_download.delay(request.data, note.id)
+        plans_download(request.data, note.id)
         return Response({"status": "ok"}, status=200)
