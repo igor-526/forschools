63c73ce5046fcdbc2c044d9e247476de476a1c6a
Igor Ter-Pogosyan <igor-526@yandex.ru>
2025-03-21 22:46:21 +0300

    TG Local server

---
+++ telegram_local_server/telegram-bot-api/build_server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -e /usr/local/bin/telegram-bot-api ]
+then
+echo "Сервер собран"
+else
+echo "Сервер не собран"
+rm -rf build
+mkdir build
+cd build
+CXXFLAGS="-stdlib=libc++" CC=/usr/bin/clang-14 CXX=/usr/bin/clang++-14 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr/local ..
+cmake --build . --target install
+fi
